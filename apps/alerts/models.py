c925ae78e2dd68cf26b66154bc2d827aed54f505
Adam Cezar Jenkins <emperorcezar@gmail.com>
2012-02-14 11:51:12 -0600

    Added level choices to alerts

--- apps/alerts/models.py
+++ apps/alerts/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce import models as tinymce_models
 
+ALERT_LEVELS = (
+    ('info','Information'),
+    ('','Warning'),
+    ('success','Success'),
+    ('error','Error'),
+    ('danger','Danger'),
+    )
+
 class Alert(models.Model):
     # This is char because there are a wide variety that can send
     sent_by = models.CharField(max_length = 200)
@@ -10,5 +18,5 @@ class Alert(models.Model):
     modified = models.DateTimeField(auto_now = True)
     title = models.CharField(max_length = 200)
     details = tinymce_models.HTMLField(blank = True)
-    level = models.CharField(max_length = 200)
+    level = models.CharField(max_length = 200, choices = ALERT_LEVELS, blank = True)
 
--- apps/springboard/views.py
+++ apps/springboard/views.py
@@ -24,7 +24,6 @@ class SpringBoard(ListView):
     def get_context_data(self, **kwargs):
         # Temporary message for testing
         from django.contrib import messages
-        messages.add_message(self.request, messages.INFO, 'Hello world.')
 
         # Call the base implementation first to get a context
         context = super(SpringBoard, self).get_context_data(**kwargs)
